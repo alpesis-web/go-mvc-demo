328b4712e1832e2ac48038c8d757a0f3c38b28fe
K <kwailamchan@hotmail.com>
2019-03-27 11:39:07 +0800

    [master] implemented login and register pages. 	modified:   packages.sh 	modified:   platform/main.go 	new file:   platform/templates/login.html 	new file:   platform/templates/register.html

--- packages.sh
+++ packages.sh
@@ -2,6 +2,7 @@ packages=(
  "github.com/gorilla/mux" 
  "github.com/gorilla/sessions" 
  "github.com/go-redis/redis"
+ "golang.org/x/crypto/bcrypt"
 )
 
 
--- platform/main.go
+++ platform/main.go
@@ -3,10 +3,11 @@ package main
 import(
     "fmt"
     "net/http"
+    "html/template"
     "github.com/gorilla/mux"
     "github.com/gorilla/sessions"
     "github.com/go-redis/redis"
-    "html/template"
+    "golang.org/x/crypto/bcrypt"
 )
 
 var client *redis.Client
@@ -23,6 +24,10 @@ func main() {
 
     r := mux.NewRouter()
     r.HandleFunc("/", indexHandler).Methods("GET")
+    r.HandleFunc("/login", loginGetHandler).Methods("GET")
+    r.HandleFunc("/login", loginPostHandler).Methods("POST")
+    r.HandleFunc("/register", registerGetHandler).Methods("GET")
+    r.HandleFunc("/register", registerPostHandler).Methods("POST")
     r.HandleFunc("/dashboard", dashboardGetHandler).Methods("GET")
     r.HandleFunc("/dashboard", dashboardPostHandler).Methods("POST")
 
@@ -37,7 +42,57 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
     templates.ExecuteTemplate(w, "index.html", nil)
 }
 
+func loginGetHandler(w http.ResponseWriter, r *http.Request) {
+    templates.ExecuteTemplate(w, "login.html", nil)
+}
+
+func loginPostHandler(w http.ResponseWriter, r *http.Request) {
+    r.ParseForm()
+    username := r.PostForm.Get("username")
+    password := r.PostForm.Get("password")
+
+    hash, err := client.Get("user:" + username).Bytes()
+    if err != nil {
+        return
+    }
+    err = bcrypt.CompareHashAndPassword(hash, []byte(password))
+    if err != nil {
+        return
+    }
+
+    session, _ := store.Get(r, "session")
+    session.Values["username"] = username
+    session.Save(r, w)
+
+    http.Redirect(w, r, "/dashboard", 302)
+}
+
+func registerGetHandler(w http.ResponseWriter, r *http.Request) {
+    templates.ExecuteTemplate(w, "register.html", nil)
+}
+
+func registerPostHandler(w http.ResponseWriter, r *http.Request) {
+    r.ParseForm()
+    username := r.PostForm.Get("username")
+    password := r.PostForm.Get("password")
+
+    cost := bcrypt.DefaultCost
+    hash, err := bcrypt.GenerateFromPassword([]byte(password), cost)
+    if err != nil {
+        return
+    }
+    client.Set("user:" + username, hash, 0)
+    http.Redirect(w, r, "/login", 302)
+}
+
 func dashboardGetHandler(w http.ResponseWriter, r *http.Request) {
+    session, _ := store.Get(r, "session")
+    _, ok := session.Values["username"]
+    if !ok {
+        http.Redirect(w, r, "/login", 302)
+        return
+    }
+
     comments, err := client.LRange("comments", 0, 10).Result()
     if err != nil {
         fmt.Printf("%v\n", err)
